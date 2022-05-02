9cd17b09139716cfd266e51979f8618b83c83d7e
rmp15 <r.parks15@imperial.ac.uk>
2022-05-02 17:37:57 -0400

    updated gitignore

--- countries/Worldwide/prog/00_bash/01_netcdf_download.sh
+++ countries/Worldwide/prog/00_bash/01_netcdf_download.sh
@@ -6,7 +6,7 @@
 clear
 
 declare -i start_day='01'
-declare -i start_month='05'
+declare -i start_month='01'
 declare -i start_year='2021'
 declare -i end_day='31'
 declare -i end_month='12'
@@ -17,4 +17,4 @@ declare -a dnames=("t2m")
 # DOWNLOAD DAILY WORLDWIDE VALUES
 #################################################
 
-python ~/git/climate/countries/Worldwide/prog/01_extract_netcdf/downloading_netcdf_files_uk_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
+python ~/git/ERA5-grids-into-national-subnational-boundaries-Worldwide/countries/Worldwide/prog/01_extract_netcdf/downloading_netcdf_files_uk_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
