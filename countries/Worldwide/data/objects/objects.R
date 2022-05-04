6f0bc50433ad95cef24ab2934ee53a7882e6028e
rmp15 <r.parks15@imperial.ac.uk>
2022-05-04 11:10:13 -0400

    added new countries

--- countries/Worldwide/data/objects/objects.R
+++ countries/Worldwide/data/objects/objects.R
@@ -11,41 +11,57 @@ countries <- c('ALB', # Albania
                'AUT', # Austria
                'BEL', # Belgium
                'BGR', # Bulgaria
+               'BWA', # Botswana
                'CAN', # Canada
                'CHE', # Switzerland
                'CHL', # Chile
+               'CMR', # Cameroon
                'CYP', # Cyprus
                'CZE', # Czech
                'DEU', # Germany
                'DNK', # Denmark
                'ESP', # Spain
                'EST', # Estonia
+               'ETH', # Ethiopia
                'FIN', # Finland
                'FRA', # France
                'GBR', # UK
                'GEO', # Georgia
+               'GHA', # Ghana
                'GRC', # Greece
                'HRV', # Croatia
                'HUN', # Hungary
                'ISL', # Iceland
                'ITA', # Italy
+               'KEN', # Kenya
                'KOR', # Republic of Korea
+               'LBR', # Liberia
                'LIE', # Liechtenstein
                'LTU', # Lithuania
+               'LSO', # Lesotho
                'LUX', # Luxembourg
                'LVA', # Latvia
                'MLT', # Malta
                'MNE', # Montenegro
+               'MWI', # Malawi
+               'NGA', # Nigeria
                'NLD', # Netherlands
                'NOR', # Norway
                'NZL', # New Zealand
                'POL', # Poland
                'PRT', # Portugal
                'ROU', # Romania
+               'RWA', # Rwanda
+               'SLE', # Sierra Leone
                'SRB', # Serbia
                'SVK', # Slovakia
                'SVN', # Slovenia
                'SWE', # Sweden
                'TWN', # Taiwan
-               'USA'  # USA
-               )
+               'TZA', # Tanzania
+               'UGA', # Uganda
+               'USA', # USA
+               'ZAF', # South Africa
+               'ZMB', # Zambia
+               'ZWE'  # Zimbabwe
+)
