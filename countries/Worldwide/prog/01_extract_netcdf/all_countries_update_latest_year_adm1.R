33c5f48da9ba2f456adec9cf27d1531a2f19c028
rmp15 <r.parks15@imperial.ac.uk>
2022-05-04 09:50:09 -0400

    added runs for all countries at once am1

---
+++ countries/Worldwide/prog/01_extract_netcdf/all_countries_update_latest_year_adm1.R
@@ -0,0 +1,24 @@
+rm(list=ls())
+
+# declare root directory, folder locations and load essential stuff
+project.folder = paste0(print(here::here()),'/')
+
+# arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+seed.arg <- as.numeric(args[1])
+
+# create grid of years an countries
+source(paste0(project.folder,'countries/Worldwide/data/objects/objects.R'))
+seed.grid = expand.grid(year=years_current,countries=countries)
+chosen.row <- seed.grid[seed.arg,]
+
+# names for files
+year <- as.numeric(chosen.row[1,1])
+dname <- 't2m'
+freq <- 'daily'
+num <- 'four'
+space.res <- '1'
+country.id <- as.character(chosen.row[1,2])
+
+# process from grids into shapefiles
+source(paste0(project.folder,'countries/Worldwide/prog/01_extract_netcdf/processing_code.R'))
