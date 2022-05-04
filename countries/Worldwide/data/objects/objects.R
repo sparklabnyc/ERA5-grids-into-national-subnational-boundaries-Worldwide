036b76e621115f4134fcd87f90086257c483aa33
rmp15 <r.parks15@imperial.ac.uk>
2022-05-04 10:41:51 -0400

    updated to run all included countries at once

--- countries/Worldwide/data/objects/objects.R
+++ countries/Worldwide/data/objects/objects.R
@@ -5,46 +5,47 @@ years_current <- c(2021:2022)
 years_total <- c(2010:2022)
 
 # list of locations currently incluing in analysis
-countries <- c('ALB', 
-               'ARM', 
-               'AUS', 
-               'AUT', 
-               'BEL',
-               'BGR', 
-               'CAN', 
-               'CHE', 
-               'CHL', 
-               'CYP',
-               'CZE', 
-               'DEU', 
-               'DNK', 
-               'ESP', 
-               'EST',
-               'FIN', 
-               'FRA', 
-               'GBR', 
-               'GEO', 
-               'GRC',
-               'HRV', 
-               'HUN', 
-               'ISL', 
-               'ITA', 
-               'KOR',
-               'LIE', 
-               'LTU', 
-               'LUX', 
-               'LVA', 
-               'MLT',
-               'MNE', 
-               'NLD', 
-               'NOR', 
-               'NZL', 
-               'POL',
-               'PRT', 
-               'ROU', 
-               'SRB', 
-               'SVK', 
-               'SVN',
-               'SWE', 
-               'TWN', 
-               'USA')
+countries <- c('ALB', # Albania
+               'ARM', # Armenia
+               'AUS', # Australia
+               'AUT', # Austria
+               'BEL', # Belgium
+               'BGR', # Bulgaria
+               'CAN', # Canada
+               'CHE', # Switzerland
+               'CHL', # Chile
+               'CYP', # Cyprus
+               'CZE', # Czech
+               'DEU', # Germany
+               'DNK', # Denmark
+               'ESP', # Spain
+               'EST', # Estonia
+               'FIN', # Finland
+               'FRA', # France
+               'GBR', # UK
+               'GEO', # Georgia
+               'GRC', # Greece
+               'HRV', # Croatia
+               'HUN', # Hungary
+               'ISL', # Iceland
+               'ITA', # Italy
+               'KOR', # Republic of Korea
+               'LIE', # Liechtenstein
+               'LTU', # Lithuania
+               'LUX', # Luxembourg
+               'LVA', # Latvia
+               'MLT', # Malta
+               'MNE', # Montenegro
+               'NLD', # Netherlands
+               'NOR', # Norway
+               'NZL', # New Zealand
+               'POL', # Poland
+               'PRT', # Portugal
+               'ROU', # Romania
+               'SRB', # Serbia
+               'SVK', # Slovakia
+               'SVN', # Slovenia
+               'SWE', # Sweden
+               'TWN', # Taiwan
+               'USA'  # USA
+               )
