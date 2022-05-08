5571c9e4dbc6b81b539865dbef945b620b31ecc6
rmp15 <r.parks15@imperial.ac.uk>
2022-05-08 13:15:35 -0400

    updated ERA5 data download

--- prog/00_bash/01_netcdf_download.sh
+++ prog/00_bash/01_netcdf_download.sh
@@ -17,4 +17,4 @@ declare -a dnames=("t2m")
 # DOWNLOAD DAILY WORLDWIDE VALUES
 #################################################
 
-python3 ~/git/ERA5-grids-into-national-subnational-boundaries-Worldwide/prog/01_extract_netcdf/downloading_netcdf_files_uk_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
+python3 ~/git/ERA5-grids-into-national-subnational-boundaries-Worldwide/prog/01_extract_netcdf/downloading_netcdf_files_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
